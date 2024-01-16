6d536fb1426935e104ecc8caad2990b07fcfb2cb
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-16 20:45:27 +0900

    4-3. String processing functions

--- python 공부.py
+++ python 공부.py
@@ -1,10 +1,18 @@
-jumin = "010606-3234567"
+python = "Python is Amazing"
+print(python.lower()) # 소문자로
+print(python.upper()) # 대문자로
+print(python[0].isupper()) # 0번쨰 자리 대문자 맞냐?
+print(len(python)) # 길이
+print(python.replace("Python", "Java")) # 대체
 
-print("성별 : " + jumin[7])
-print('연 : ' + jumin[0:2]) # 0번째부터 2번째 직전까지
-print('월 : ' + jumin[2:4])
-print('일 : ' + jumin[4:6])
+index = python.index("n") # n이 몇번째 자리에 있느냐
+print(index)
+index = python.index("n", index + 1) # index에서 찾은자리 + 1 자리부터 시작
+print(index)
 
-print("생년월일 : " + jumin[:6]) # 처음부터 6번째 직전까지
-print("뒤 7자리 : " + jumin[7:])
-print("뒤 7자리 (뒤에부터) : " + jumin[-7:]) # 맨 끝 수는 -1번째로 볼 수 있다
+print(python.find("n")) # n이 몇번째 자리에 있느냐
+print(python.find("Java")) # find는 내가 원하는 값이 없을 떄 -1을 반환
+print(python.index("Java")) # index는 내가 원하는 값이 없을 떄 error 발생, 뒤에 것 진행 못함
+print("hi")
+
+print(python.count("n")) # n이 몇 번 등장하느냐
