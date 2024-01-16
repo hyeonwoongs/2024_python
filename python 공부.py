4a27aee90c4e23634f3ca51dbe1ce890e5bdc754
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-16 18:14:13 +0900

    3-4. random function

--- python 공부.py
+++ python 공부.py
@@ -1,11 +1,9 @@
-print(abs(-5)) # 절대값
-print(pow(4, 2)) # 4^2
-print(max(5, 12)) # 최대값
-print(min(5, 12)) # 최소값
-print(round(3.14)) # 반올림
-print(round(4.99)) # 반올림
+from random import * # python에서 제공하는 math library 안에 있는 모든 것을 이용하겠다
 
-from math import * # python에서 제공하는 math library 안에 있는 모든 것을 이용하겠다
-print(floor(4.99)) # 내림
-print(ceil(3.14)) # 올림
-print(sqrt(25)) # 제곱근
+print(random()) # 0.0 ~ 1.0 미만의 임의의 값 생성
+print(random() * 10) # 0.0 ~ 10.0 미만의 임의의 값 생성
+print(int(random() * 10)) # 0 ~ 10 미만의 임의의 값 생성
+print(int(random() * 10) + 1) # 1 ~ 10 이하의 임의의 값 생성
+print(int(random() * 45) + 1) # 1 ~ 45 이하의 임의의 값 생성
+print(randrange(1, 46)) # 1 ~ 46 미만의 임의의 값 생성
+print(randint(1, 45)) # 1 ~ 45 이하의 임의의 값 생성
