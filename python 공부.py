0c95105999dd96c311e97acc4aa05a5ca93e06f7
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-16 18:03:55 +0900

    3-3. Numeric processing functions

--- python 공부.py
+++ python 공부.py
@@ -1,16 +1,11 @@
-print(2 + 3 * 4)
-print((2 + 3) * 4)
-number = 2 + 3 * 4
-print(number)
-number = number + 2
-print(number)
-number += 2 # number = number + 2 와 같은 뜻
-print(number)
-number *= 2
-print(number)
-number /= 2
-print(number)
-number -= 2
-print(number)
-number %= 5
-print(number)
+print(abs(-5)) # 절대값
+print(pow(4, 2)) # 4^2
+print(max(5, 12)) # 최대값
+print(min(5, 12)) # 최소값
+print(round(3.14)) # 반올림
+print(round(4.99)) # 반올림
+
+from math import * # python에서 제공하는 math library 안에 있는 모든 것을 이용하겠다
+print(floor(4.99)) # 내림
+print(ceil(3.14)) # 올림
+print(sqrt(25)) # 제곱근
