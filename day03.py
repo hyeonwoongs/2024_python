b8c39c120e835e5abc911d86c2ec70cb6db4d699
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-16 15:06:40 +0900

    v2.9 prime number day03

--- day03.py
+++ day03.py
@@ -1,13 +1,10 @@
-univ = "inha"
-i = 0
-while i < len(univ):
-    print(univ[i], end=' ')
-    i = i + 1
 # prime number
 number = int(input("Input number : "))
 is_prime = True
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-print()
 if number < 2:
     print(f'{number} is NOT prime number!')
 else:
@@ -15,17 +12,17 @@ else:
         if number % i == 0:
             is_prime = False
             break
+for number in range(n1, n2+1):
+    is_prime = True
 
-for letter in univ:
-    print(letter, end=' ')
-
-print()
-
-#for k in range(0, len(univ), 1):
-#for k in range(0, len(univ)):
-for k in range(len(univ)):
-    print(univ[k], end=' ')
     if is_prime:
         print(f'{number} is prime number')
+    if number < 2:
+        pass
     else:
         print(f'{number} is NOT prime number!')
+        for i in range(2, number):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime: print(number, end=' ')
