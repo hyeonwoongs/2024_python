c027feb5892eb9cc46f230616c35a6220dd3d10c
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-16 22:30:27 +0900

    Quiz #3

--- python 공부.py
+++ python 공부.py
@@ -1,20 +1,18 @@
-# \n : 줄바꿈
-print("백문이 불여일견\n백견이 불여일타")
+# Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+#
+# 예) http://naver.com
+# 규칙1 : http:// 부분은 제외 => naver.com
+# 규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+# 규칙3 : 남은 글자 중 처음 세자리 + 글자 개수 + 글자 내 'e' 개수 + "!" 로 구성 \
+#               (nav)              (5)           (1)         (!)
+# 예) 생성된 비밀번호 : nav51!
 
-# 저는 "나도코딩"입니다. \", \' : 문장 내에서 따옴표
-print("저는 '나도코딩'입니다.")
-print('저는 "나도코딩"입니다.')
-print("저는 \"나도코딩\"입니다.")
-print("저는 \'나도코딩\'입니다.")
 
-# \\ : 문장 내에서 \
-print("C:\\2024_python\\python 공부.py")
 
-# \r : 커서를 맨 앞으로 이동
-print("Red Apple\rPine") # visual studio 에서는 PineApple로 출력
-
-# \b : 백스페이스 (한 글자 삭제)
-print("Redd\bApple")
-
-# \t : 탭 (4칸 이동)
-print("Red\tApple") # visual studio 에서는 4칸 이동
+url = "http://naver.com"
+my_str = url.replace("http://", "")
+# print(my_str)
+my_str = my_str[:my_str.index(".")]
+# print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0}의 비밀번호는 {1}입니다.".format(url, password))
