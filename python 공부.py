f7cb0aaf5f06fcc23c3a3f24322a9e6b4a0d15bb
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-17 14:31:13 +0900

    3.9 day04

--- python 공부.py
+++ python 공부.py
@@ -1,18 +1,19 @@
-# Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
-#
-# 예) http://naver.com
-# 규칙1 : http:// 부분은 제외 => naver.com
-# 규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
-# 규칙3 : 남은 글자 중 처음 세자리 + 글자 개수 + 글자 내 'e' 개수 + "!" 로 구성 \
-#               (nav)              (5)           (1)         (!)
-# 예) 생성된 비밀번호 : nav51!
+# 리스트 []
 
+# 지하철 칸별로 10명, 20명, 30명
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
 
+subway = [10, 20, 30]
+print(subway)
 
-url = "http://naver.com"
-my_str = url.replace("http://", "")
-# print(my_str)
-my_str = my_str[:my_str.index(".")]
-# print(my_str)
-password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
-print("{0}의 비밀번호는 {1}입니다.".format(url, password))
+subway = ["유재석", "조세호", "박명수"]
+print(subway)
+
+# 조세호씨가 몇 번째 칸에 타고 있는가?
+print(subway.index("조세호"))
+
+# 하하씨가 다음 정류장에서 다음 칸에 탐
+subway.append("하하")
+print(subway)
