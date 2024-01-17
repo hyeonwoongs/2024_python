d55f77553d3014120f3562accb0ba209b07caa94
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-17 09:13:50 +0900

    v1.5 assignment day03

--- day03.py
+++ day03.py
@@ -1,21 +1,16 @@
-# prime number
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
+# Assignment (loop)
+# (100°F − 32) × 5/9 = 37.778°C
+# (0°C × 9/5) + 32 = 32°F
 
-if n1 > n2:
-    n1, n2 = n2, n1
+while True:
+    menu = input("1) Fahrenheit -> Celsius   2) Celsius -> Fahrenheit   3) Quit program : ")
 
-for number in range(n1, n2+1):
-    is_prime = True
-
-    if number < 2:
-        pass
-        #pass
-        continue
-    else:
-        for i in range(2, number):
-            if number % i == 0:
-                is_prime = False
-                break
-        if is_prime: print(number, end=' ')
+    if menu == '1':
+        fahrenheit = float(input('Input Fahrenheit : '))
+        print(f'Fahrenheit : {fahrenheit}F, Celsius : {((fahrenheit-32.0)*5.0/9.0):.4f}C')
+    elif menu == '2':
+        celsius = float(input('Input Celsius : '))
+        print(f'Celsius : {celsius}C, Fahrenheit : {((celsius*9.0/5.0)+32.0):.4f}F')
+    elif menu == '3':
+        print('Terminate Program.')
+        break
