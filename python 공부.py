9208678fc43e834de185c42ee1bf0dcc94e7e81a
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-17 16:40:34 +0900

    5-4. set

--- python 공부.py
+++ python 공부.py
@@ -1,13 +1,27 @@
-menu = ("돈까스", "치즈까스")
-print(menu[0])
-print(menu[1])
+# 집합 (set)
+# 중복 안됨, 순서 없음
+my_set = {1,2,3,3,3}
+print(my_set)
 
-# menu.add("생선까스") tuple은 값을 추가할 수 없고 고정된 값만 사용 가능하다
+java = {"유재석", "김태호", "양세형"}
+python = {"유재석", "박명수"}
 
-name = "김종국"
-age = 20
-hobby = "코딩"
-print(name, age, hobby)
+# 교집합 (java와 python을 모두 할 수 있는 개발자)
+print(java & python)
+print(java.intersection(python))
 
-(name, age, hobby) = ("김종국", 20, "코딩")
-print(name,age,hobby)
+# 합집합 (java 또는 python을 할 수 있는 개발자)
+print(java | python)
+print(java.union(python))
+
+# 차집합 (java를 할 수 있지만 python은 할 줄 모르는 개발자)
+print(java - python)
+print(java.difference(python))
+
+# python을 할 줄 아는 사람이 늘어남
+python.add("김태호")
+print(python)
+
+# java를 잊었어요
+java.remove("김태호")
+print(java)
