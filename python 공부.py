62b0b26c685badf34e44a38dcc0220036b5522fb
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-18 13:00:36 +0900

    6-2. for

--- python 공부.py
+++ python 공부.py
@@ -1,18 +1,14 @@
-# weather = "비"
-# weather = input("오늘 날씨는 어때요? ")
-# if weather == "비" or weather == "눈":
-#     print("우산을 챙기세요")
-# elif weather == "미세먼지": # 다른 조건
-#     print("마스크를 챙기세요")
-# else: # 모든 조건이 맞지 않을 때
-#     print("준비물 필요 없어요.")
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
 
-temp = int(input("기온은 어떄요? "))
-if 30 <= temp:
-    print("너무 더워요. 나가지 마세요")
-elif 10 <= temp and temp < 30:
-    print("괜찮은 날씨에요")
-elif 0 <= temp < 10:
-    print("외투를 챙기세요")
-else:
-    print("너무 추워요. 나가지 마세요")
+# for waiting_no in [0, 1, 2, 3, 4]:
+#     print("대기번호 : {0}".format(waiting_no))
+
+for waiting_no in range(1, 6):
+    print("대기번호 : {0}".format(waiting_no))
+
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer) )
