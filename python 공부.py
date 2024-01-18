4bb5960312a9d44ac1039966719454d1e78bfda3
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-18 15:08:09 +0900

    6-4 continue & break

--- python 공부.py
+++ python 공부.py
@@ -1,21 +1,10 @@
-# # while
-# customer = "토르"
-# index = 5
-# while index >= 1: # 어떤 조건이 만족할떄 까지 반복하라
-#     print("{0}, 커피가 준비 되었습니다. {1} 번 남았아요.".format(customer, index))
-#     index -= 1
-#     if index == 0:
-#         print("커피는 폐기처분되었습니다.")
+abscent = [2, 5] # 결석
+no_book = [7] # 책을 깜빡했음
+for student in range(1, 11): # 1,2,3,4,5,6,7,8,9,10
+    if student in abscent:
+        continue # 반복문 계속 진행
+    elif student in no_book:
+        print("오늘 수업 여기까지.{0}는 교무실로 따라와".format(student))
+        break # 반복문 끝냄
+    print("{0}, 책을 읽어봐".format(student))
 
-# customer = "아이언맨"
-# index = 1
-# while True:
-#     print("{0}, 커피가 준비되었습니다. 호출 {1} 회".format(customer, index))
-#     index += 1
-
-customer = "토르"
-person = "Unknown"
-
-while person != customer :
-    print("{0}, 커피가 준비 되었습니다.".format(customer))
-    person = input("이름이 어떻게 되세요? ")
