fa4aeedc779dbefae05ecafd1f22ae2dfa511f6c
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-18 12:46:49 +0900

    6-1. if

--- python 공부.py
+++ python 공부.py
@@ -1,42 +1,18 @@
-# Quiz) 당신의 학교에서는 파이썬 코딩 대회를 주최합니다.
-# 참석률을 높이기 위해 댓글 일벤트를 진행하기로 하였습니다.
-# 댓글 작성자들 중에 추첨을 통해 1명은 치킨, 3명은 커피 쿠폰을 받게 됩니다.
-# 추첨 프로그램을 작성하시오.
-#
-# 조건1 : 편의상 댓글은 20명이 작성하였고 아이디는 1~20 이라고 가정
-# 조건2 : 댓글 내용과 상관 없이 무작위로 추첨하되 중복 불가
-# 조건3 : random 모듈의 shuffle 과 sample 을 활용
-#
-# # (출력 예제)
-# # -- 당첨자 발표 --
-# # 치킨 당첨자 : 1
-# # 커피 당첨자 : [2,3,4]
-# # -- 축하합니다 --
-#
-# (활용 예제)
-# from random import *
-# lst = [1,2,3,4,5]
-# print(lst)
-# shuffle(lst)
-# print(lst)
-# print(sample(lst,1))
+# weather = "비"
+# weather = input("오늘 날씨는 어때요? ")
+# if weather == "비" or weather == "눈":
+#     print("우산을 챙기세요")
+# elif weather == "미세먼지": # 다른 조건
+#     print("마스크를 챙기세요")
+# else: # 모든 조건이 맞지 않을 때
+#     print("준비물 필요 없어요.")
 
-
-
-
-from random import *
-users = range(1,21)
-print(type(users))
-users = list(users)
-print(type(users))
-
-print(users)
-shuffle(users)
-print(users)
-
-winners = sample(users,4)
-
-print("--당첨자 발표--")
-print("치킨 당첨자 : {0}".format(winners[0]))
-print("커피 당첨자 : {0}".format(winners[1:]))
-print("--축하합니다--")
+temp = int(input("기온은 어떄요? "))
+if 30 <= temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10 <= temp and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
