a5c91e6b7e025b821eeacf177f2123465cc6307b
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-18 14:12:53 +0900

    6-3. while

--- python 공부.py
+++ python 공부.py
@@ -1,14 +1,21 @@
-# print("대기번호 : 1")
-# print("대기번호 : 2")
-# print("대기번호 : 3")
-# print("대기번호 : 4")
+# # while
+# customer = "토르"
+# index = 5
+# while index >= 1: # 어떤 조건이 만족할떄 까지 반복하라
+#     print("{0}, 커피가 준비 되었습니다. {1} 번 남았아요.".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 폐기처분되었습니다.")
 
-# for waiting_no in [0, 1, 2, 3, 4]:
-#     print("대기번호 : {0}".format(waiting_no))
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비되었습니다. 호출 {1} 회".format(customer, index))
+#     index += 1
 
-for waiting_no in range(1, 6):
-    print("대기번호 : {0}".format(waiting_no))
+customer = "토르"
+person = "Unknown"
 
-starbucks = ["아이언맨", "토르", "아이엠 그루트"]
-for customer in starbucks:
-    print("{0}, 커피가 준비되었습니다.".format(customer) )
+while person != customer :
+    print("{0}, 커피가 준비 되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요? ")
