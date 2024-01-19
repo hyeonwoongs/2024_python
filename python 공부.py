05862b8db708597b8cb36c8d3bcf32ac2949b22a
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-19 18:40:13 +0900

    7-4. keword value

--- python 공부.py
+++ python 공부.py
@@ -1,13 +1,5 @@
-# def profile(name, age, main_lang):
-#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
-#
-# profile("유재석",20,"파이썬")
-# profile("김태호",25,"자바")
+def profile(name, age, main_lang):
+    print(name, age, main_lang)
 
-# 같은 학교 같은 학년 같은 반 같은 수업
-
-def profile(name, age = 17, main_lang = "파이썬"):
-    print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
-
-profile("유재석")
-profile("김태호")
+profile(name = "유재석", main_lang = "파이썬", age = 20)
+profile(main_lang = "자바", age = 25, name = "김태호")
