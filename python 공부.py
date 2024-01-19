9b1de7646ffc90435a60e50817570808b2cc3b0c
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-19 14:03:30 +0900

    7-2. arguments and return values

--- python 공부.py
+++ python 공부.py
@@ -7,24 +7,25 @@ def deposit(balance, money): # 입금
     print("입금이 완료되었습니다. 잔액은 {0}원입니다.".format(balance+money))
     return balance + money
 
-balance = 0 # 초기 잔액
-balance = deposit(balance, 1000) # 1,000원 입금
-print(balance) # 반환값 저장
+# balance = 0 # 초기 잔액
+# balance = deposit(balance, 1000) # 1,000원 입금
+# print(balance) # 반환값 저장
 
 def withdraw(balance, money): # 출금
     if balance >= money: # 잔액이 출금보다 많으면
-        print("출금이 완료되었습니다. 잔액은 {0}원입니다.".format(balance - money))
+        print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
         return balance - money
     else:
-        print("출금이 완료되지 않았습니다.".format(balance))
+        print("출금이 완료되지 않았습니다. 잔액은 {0} 원입니다.".format(balance))
         return balance
 
-balance = 0 # 초기 잔액
-balance = dep(balance, 1000) # 1,000원 입금
-print(balance) # 반환값 저장
+def withdraw_night(balance, money): #저녁에 출금
+    commission = 100 # 수수료 100원
+    return commission, balance - money - commission
 
-  #def 함수명(전달값1, 전달값2, ...):
-#       실행할 문장1
-#       실행할 문장2
-#       ...
-#       return 반환값 반환값이 여러 개인 경우 쉼표로 구분해 튜플 형태로 반환
+balance = 0 # 초기 잔액
+balance = deposit(balance, 1000) # 1,000원 입금
+# balance = withdraw(balance, 2000)
+# balance = withdraw(balance, 500)
+commission, balance = withdraw_night(balance, 500)
+print("수수료 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
