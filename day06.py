a835c3dffb1bc949cf457dcff0dcc4cd5471e6d0
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-19 15:28:11 +0900

    v6.4 day06

--- day06.py
+++ day06.py
@@ -1,24 +1,26 @@
 class FlyingMixin:
     def fly(self):
         return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~"
-
 class SwimmingMixin:
     def swim(self):
         return f"{self.name}이(가) 수영을 합니다."
-
 class Pokemon:
     def __init__(self, name):
         self.name = name
-
     def attack(self):
         print("공격~")
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, new_name):
+        self.name = new_name
 
 class Charizard(Pokemon, FlyingMixin):
     pass
 
 class Gyarados(Pokemon, SwimmingMixin):
     pass
-
 g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
 # print(c1.fly())
@@ -26,6 +28,6 @@ c1 = Charizard("리자몽")
 # c1.attack()
 # #Charizard.attack()
 # Charizard.attack(c1)
-print(g1.name)
-g1.name = "잉어킹"
-print(g1.name)
+print(g1.get_name())
+g1.set_name("잉어킹")
+print(g1.get_name())
