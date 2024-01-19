90cd3ac52c7aff02abd00c3d415785ed6576d493
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-19 15:33:16 +0900

    v6.6 day06

--- day06.py
+++ day06.py
@@ -1,40 +1,39 @@
 class FlyingMixin:
     def fly(self):
         return f"{self.hidden_name}이(가) 하늘을 훨훨 날아갑니다~"
+class SwimmingMixin:
+    def swim(self):
+        return f"{self.hidden_name}이(가) 수영을 합니다."
+class Pokemon:
+    def __init__(self, name):
+        self.hidden_name = name
+    def attack(self):
+        print("공격~")
 
-    class SwimmingMixin:
-        def swim(self):
-            return f"{self.hidden_name}이(가) 수영을 합니다."
 
-        class Pokemon:
-            def __init__(self, name):
-                self.hidden_name = name
+    @property
+    def name(self):
+        return self.hidden_name
 
-            def attack(self):
-                print("공격~")
 
-            def get_name(self):
-                return self.hidden_name
+    @name.setter
+        self.hidden_name = new_name
 
-            def set_name(self, new_name):
-                self.hidden_name = new_name
+    name = property(get_name, set_name)
+    #name = property(get_name, set_name)
 
-            name = property(get_name, set_name)
 
-            class Charizard(Pokemon, FlyingMixin):
-                pass
-
-            class Gyarados(Pokemon, SwimmingMixin):
-                pass
-
-            g1 = Gyarados("갸라도스")
-            c1 = Charizard("리자몽")
-
+class Charizard(Pokemon, FlyingMixin):
+    pass
+class Gyarados(Pokemon, SwimmingMixin):
+    pass
+g1 = Gyarados("갸라도스")
+c1 = Charizard("리자몽")
 # print(g1.get_name())
 # g1.set_name("잉어킹")
 # print(g1.get_name())
 
-# property
+# property 2nd
 print(g1.name)
 g1.name = "잉어킹"
 print(g1.name)
