2a57474626587e6d56eead61de3e7618d486cc0f
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-19 11:27:29 +0900

    v5.6 day06

--- python 공부.py
+++ python 공부.py
@@ -1,4 +1,16 @@
 def open_account():
-    print("새로운 계좌가 생성되었습니다.") # 여기까지는 정의만 해둔 것
+    print("새로운 계좌를 개설합니다.")
 
-open_account() # 함수를 호출 해야지 기능을 수행함
+open_account()
+
+def deposit(balance, money): # 입금 처리 함수
+    print("{0}원을 입금했습니다. 잔액은 {1}원입니다.".format(money, balance+money)) #balance 잔액을 의미
+    return balance + money # 입금 후 잔액 반환
+
+balance = 0 # 초기 잔액
+balance = deposit(balance, 1000) # 1,000원 입금
+  #def 함수명(전달값1, 전달값2, ...):
+#       실행할 문장1
+#       실행할 문장2
+#       ...
+#       return 반환값 반환값이 여러 개인 경우 쉼표로 구분해 튜플 형태로 반환
