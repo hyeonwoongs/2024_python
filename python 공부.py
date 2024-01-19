9873f00bb335ca9e6ff1c84c78f3585e04bc68f4
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-19 13:46:31 +0900

    v5.9 day06

--- python 공부.py
+++ python 공부.py
@@ -3,12 +3,26 @@ def open_account():
 
 open_account()
 
-def deposit(balance, money): # 입금 처리 함수
-    print("{0}원을 입금했습니다. 잔액은 {1}원입니다.".format(money, balance+money)) #balance 잔액을 의미
-    return balance + money # 입금 후 잔액 반환
+def deposit(balance, money): # 입금
+    print("입금이 완료되었습니다. 잔액은 {0}원입니다.".format(balance+money))
+    return balance + money
 
 balance = 0 # 초기 잔액
 balance = deposit(balance, 1000) # 1,000원 입금
+print(balance) # 반환값 저장
+
+def withdraw(balance, money): # 출금
+    if balance >= money: # 잔액이 출금보다 많으면
+        print("출금이 완료되었습니다. 잔액은 {0}원입니다.".format(balance - money))
+        return balance - money
+    else:
+        print("출금이 완료되지 않았습니다.".format(balance))
+        return balance
+
+balance = 0 # 초기 잔액
+balance = dep(balance, 1000) # 1,000원 입금
+print(balance) # 반환값 저장
+
   #def 함수명(전달값1, 전달값2, ...):
 #       실행할 문장1
 #       실행할 문장2
