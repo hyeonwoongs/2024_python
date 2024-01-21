df69767e2857aba307fc84bdf9090911a4bbdfdd
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-21 17:12:50 +0900

    8-1. Standard Input/Output

--- python 공부.py
+++ python 공부.py
@@ -1,17 +1,22 @@
-gun = 10
+# print("Python", "Java", sep=",", end="?")
+# print("무엇이 더 재미있을까요?")
 
-def checkpoint(soldiers): # 경계근무
-    #gun = 20
-    global gun # 전역 공간에 있는 gun 사용
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {0}".format(gun))
+# import sys
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stderr)
 
-def checkpoint_ret(gun, soldiers):
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {0}".format(gun))
-    return gun
+# 시험 성적
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items():
+#     # print(subject, score)
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")
 
-print("전체 총 : {0}".format(gun))
-#checkpoint(2) # 2명이 경계 근무 나감
-gun = checkpoint_ret(gun,2)
-print("남은 총 : {0}".format(gun))
+# 은행 대기순번표
+# 001, 002, 003, ...
+# for num in range(1,21):
+#     print("대기번호 : " + str(num).zfill(3))
+
+answer = input("아무 값이나 입력하세요 : ")
+# answer = 10
+# print(type(answer))
+print("입력하신 값은 " + answer + "입니다.")
