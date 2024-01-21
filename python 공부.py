46d8e806c88928ef44ebdf61957e1d67fcaf49c9
hyeonwoongs <hyeonwoongs@gmail.com>
2024-01-21 21:45:59 +0900

    8-3. File input/output

--- python 공부.py
+++ python 공부.py
@@ -1,19 +1,35 @@
-# 빈 자리는 빈공간으로 두고, 오른쪽 정렬을 하되, 총 10자리 공간을 확보
-print("{0: >10}".format(500))
-# 양수일 땐 +로 표시, 음수일 땐 -로 표시
-print("{0: >+10}".format(500))
-print("{0: >+10}".format(-500))
-# 왼쪽 정렬하고, 빈칸으로 _로 채움
-print("{0:_<10}".format(500))
-# 3자리 마다 콤마를 찍어주기
-print("{0:,}".format(10000000000000000))
-# 3자리 마다 콤마를 찍어주기, +- 부호도 붙이기
-print("{0:+,}".format(10000000000000000))
-print("{0:+,}".format(-10000000000000000))
-# 3자리 마다 콤마를 찍어주기, 부호도 붙이고, 자릿수 확보하기
-# 돈이 많으면 행복하니까 빈 자리는 ^로 채워주기
-print("{0:^<+30}".format(-10000000000000000))
-# 소수점 출력
-print("{0:f}".format(5/3))
-# 소수점 특정 자리수 까지만 표시 (소수점 3째 자리에서 반올림)
-print("{0:.2f}".format(5/3))
+# score_file = open("score.txt", "w", encoding="utf8") #utf8을 정의해주지 않으면 한글을 썼을 때 좀 이상한 문자로 적힌 경우가 생길 수 있음
+# print("수학 : 0",file=score_file)
+# print("영어 : 50",file=score_file)
+# score_file.close()
+#
+# score_file = open("score.txt", "a",encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
+#
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline()) # 줄별로 읽기, 한 줄 읽고 커서는 다음 줄로 이동
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
+#
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() # list 형태로 저장
+for line in lines:
+    print(line, end="")
+
+score_file.close()
